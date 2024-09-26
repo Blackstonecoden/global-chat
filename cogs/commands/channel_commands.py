e3504b35f04f71cb96a76333d00f50a24a3c531c
blackstone12 <blackstone_12@outlook.de>
2024-09-26 14:30:52 +0200

    translateable parameters

--- cogs/commands/channel_commands.py
+++ cogs/commands/channel_commands.py
@@ -13,12 +13,14 @@ class channel_commands(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    channel_command = app_commands.Group(name=discord.app_commands.locale_str("command_channel"), description=discord.app_commands.locale_str("command_channel_description"), default_permissions=discord.Permissions(administrator=True), guild_only=True)
+    channel_command = app_commands.Group(name=discord.app_commands.locale_str("channel"), description=discord.app_commands.locale_str("channel_description"), default_permissions=discord.Permissions(administrator=True), guild_only=True)
 
-    @channel_command.command(name=discord.app_commands.locale_str("command_channel_set"), description=discord.app_commands.locale_str("command_channel_set_description"))
-    @app_commands.describe(channel=discord.app_commands.locale_str("command_channel_set_describe_channel"))
+    @channel_command.command(name=discord.app_commands.locale_str("channel_set"), description=discord.app_commands.locale_str("channel_set_description"))
+    @app_commands.describe(channel=discord.app_commands.locale_str("channel_set_describe_channel"))
+    @app_commands.rename(channel=discord.app_commands.locale_str("param_channel"))
     async def channel_set(self, interaction: discord.Interaction, channel: discord.TextChannel = None):
-        await interaction.response.send_message(translator.translate(interaction.locale.value, "hello", user_id=interaction.user.id, username=interaction.user.name), ephemeral=True)
-
+        if not channel:
+            channel = interaction.channel
+        await interaction.response.send_message(f"<#{channel.id}>", ephemeral=True)
 async def setup(client:commands.Bot) -> None:
     await client.add_cog(channel_commands(client))
